a5a5b39d81926d0a9eef1d11d4810df74de6fe74
Lanjar <107414679+Lattae30@users.noreply.github.com>
2022-07-12 14:44:13 +0700

    Delete src directory

--- src/java/Query/ProductQuery.java
+++
@@ -1,8 +0,0 @@
-package Query;
-
-public class ProductQuery {
-    public String get = "SELECT * FROM clothes";
-    public String getByName =  "SELECT * FROM clothes where name LIKE ?";
-    public String getUser = "SELECT * FROM users";
-    public String getUserName =  "SELECT * FROM users where u_name LIKE ?";
-}
